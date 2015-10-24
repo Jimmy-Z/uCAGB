5e15d44979965b3796744edf042b88b61c18b203
JimmyZ <jimmy.z.z@gmail.com>
2015-10-24 17:11:35 +0800

    upload/download done

--- common/common.h
+++ common/common.h
@@ -15,3 +15,26 @@
 #define CMD_UNSET_WS	(6 << CMD_FLAG_BITS)
 
 #define BULK_SIZE 8 // u32[8]
+
+// these are shared between DFAGB and PC
+// commands sent to DFAGB from PC, the lower 24 bits are used as 2 parameters
+// one 8 bit and one 16 bit
+// upload and download (to/from the 128KB buffer) are processed by the SIO FSM
+#define DF_CMD_UPLOAD	(1 << 24)
+#define DF_CMD_DOWNLOAD	(2 << 24)
+// actually DFAGB FSM only check the 1st byte
+// so make sure the higher 8 bit is not a valid command
+#define DF_CMD_NOP	0x00504f4e
+// these are WORKER commands, SIO FSM will transfer them to WORKER FSM
+#define DF_CMD_FLASH	(3 << 24)
+#define DF_CMD_DUMP	(4 << 24)
+#define DF_CMD_READ_SRAM	(5 << 24)
+#define DF_CMD_WRITE_SRAM	(6 << 24)
+#define DF_CMD_READ_FLASH	(7 << 24)
+#define DF_CMD_WRITE_FLASH	(8 << 24)
+#define DF_CMD_READ_EEPROM	(9 << 24)
+#define DF_CMD_WRITE_EEPROM	(0x0a << 24)
+
+// this is the 4CC status code PC will read from DFAGB via SIO
+#define DF_STATE_IDLE	0x454c4449
+#define DF_STATE_BUSY	0x59535542
--- dfagb/source/dfagb.c
+++ dfagb/source/dfagb.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../../common/common.h"
 #include "../../common/crc32.h"
 
 const char sTitle[] = "DFAGB - Dumper/Flasher for GBA build %s %s\n";
@@ -16,29 +17,112 @@ void irq_keypad(void){
 	SystemCall(0x26);
 }
 
-// serial processings
-u32 so_data = 0;
-u32 si_data;
-
-void start_serial(){
-	REG_SIODATA32 = so_data;
-	REG_SIOCNT &= ~(SIO_SO_HIGH);
-	REG_SIOCNT |= SIO_START;
-	REG_SIOCNT |= SIO_SO_HIGH;
-}
-
-void irq_serial(void){
-	si_data = REG_SIODATA32;
-	iprintf("\nSIODATA32: 0x%08x", si_data);
-	++ so_data;
-	start_serial();
-}
-
+// 128K bytes buffer, thats
 #define BUF_SIZE 0x20000
 EWRAM_BSS u8 buf[BUF_SIZE];
 #define buf16 ((u16*)buf)
 #define buf32 ((u32*)buf)
 u32 crc32_table[CRC32_TABLE_LEN];
+
+// SIO FSM stat and parameters
+#define SIO_IDLE 0
+#define SIO_UPLOAD 1
+#define SIO_DOWNLOAD 2
+u32 sio_state, sio_p0, sio_p1, sio_p3, sio_p4;
+
+void start_serial(u32 out32){
+	REG_SIODATA32 = out32;
+	REG_SIOCNT &= ~(SIO_SO_HIGH);
+	REG_SIOCNT |= SIO_START;
+	REG_SIOCNT |= SIO_SO_HIGH;
+}
+
+/*
+upload 4 example
+===
+	s == idle, out == idle
+00010004	<->	<IDLE>
+	s = upload, s0 = 4, p1 = 0
+<payload0>	<->	<undefined>
+	buf[0] = payload0, p1 = 1
+<payload1>	<->	<undefined>
+	buf[1] = payload1, p1 = 2
+<payload2>	<->	<undefined>
+	buf[2] = payload2, p1 = 3
+<payload3>	<->	<undefined>
+	buf[3] = payload3, p1 = 4, s = idle, out = idle
+<nop>		<->	<IDLE>
+
+download 4 example
+===
+	s == idle, out == idle
+00020004	<->	<IDLE>
+	s = download, s0 = 4, p1 = 0, out = buf[0], p1 = 1
+<undefined>	<->	buf[0]
+	out = buf[1], p1 = 2
+<undefined>	<->	buf[1]
+	out = buf[2], p1 = 3
+<undefined>	<->	buf[2]
+	out = buf[3], p1 = 4
+<undefined>	<->	buf[3]
+	s = idle, out = idle
+<nop>		<-> 	<IDLE>
+*/
+void irq_serial(void){
+	u32 in32 = REG_SIODATA32, out32 = DF_STATE_IDLE;
+	switch(sio_state){
+		case SIO_IDLE:
+			switch(in32 & 0xff000000){
+				case DF_CMD_UPLOAD:
+					// upload to buf
+					sio_state = SIO_UPLOAD;
+					// length, of u32
+					sio_p0 = in32 & 0x0000ffff;
+					iprintf("\nstart uploading %d bytes", sio_p0 << 2);
+					// current offset
+					sio_p1 = 0;
+					// during upload, the PC side doesn't care about data they receive
+					break;
+				case DF_CMD_DOWNLOAD:
+					// download to buf
+					sio_state = SIO_DOWNLOAD;
+					// length, of u32
+					sio_p0 = in32 & 0x0000ffff;
+					iprintf("\nstart downloading %d bytes", sio_p0 << 2);
+					// current offset
+					sio_p1 = 0;
+					// PC is expecting data on the next return
+					out32 = buf32[sio_p1++];
+					break;
+				// case DF_CMD_FLASH:
+				// case DF_CMD_NOP & 0xff000000:
+				default:
+					if(in32 != DF_CMD_NOP){
+						iprintf("\ninvalid command: 0x%08x", in32);
+					}
+					break;
+			}
+			break;
+		case SIO_UPLOAD:
+			buf32[sio_p1++] = in32;
+			if(sio_p1 >= sio_p0){
+				sio_state = SIO_IDLE;
+				iprintf("\nupload complete");
+			}
+			break;
+		case SIO_DOWNLOAD:
+			if(sio_p1 < sio_p0){
+				out32 = buf32[sio_p1++];
+			}else{
+				sio_state = SIO_IDLE;
+				iprintf("\ndownload complete");
+			}
+			break;
+	}
+	start_serial(out32);
+}
+
+// WORKER FSM and states
 
 int main(void) {
 	// keypad setup
@@ -54,7 +138,8 @@ int main(void) {
 	irqSet(IRQ_SERIAL, irq_serial);
 	irqEnable(IRQ_VBLANK | IRQ_KEYPAD | IRQ_SERIAL);
 
-	start_serial();
+	sio_state = SIO_IDLE;
+	start_serial(DF_STATE_IDLE);
 
 	// console setup, see libdgba/src/console.c for details
 	consoleInit(0, 4, 0, NULL, 0, 15);
